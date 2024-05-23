795682c2eed4e36e1848201be19bc87f83129fb5
Jasraj Gill <jasrajgill2752@gmail.com>
2024-05-23 16:48:02 +0930

    ioj

--- migrations/V1.1.2__schema_initial_objects.sql
+++ migrations/V1.1.2__schema_initial_objects.sql
@@ -1,11 +1,1 @@
-ALTER TABLE HELLO_WORLD ADD COLUMN AGE_OLD NUMBER;
-ALTER TABLE HELLO_WORLD ADD COLUMN GENDER_OLD VARCHAR;
-ALTER TABLE HELLO_WORLD ADD COLUMN EMAIL VARCHAR;
-
-INSERT INTO HELLO_WORLD 
-VALUES ('Joe', 'Smith', 32, 'F', 'joe.smith@@@XYZ.com'),
-('Alice', 'ohnson', 28, 'F', 'lice.johnson@@@XYZ.com'),
-('Bob', 'Williams', 35, 'M', 'bob.williams@@@XYZ.com'),
-('Charlie', 'Brown', 37, 'M', 'charlie.brown@@@XYZ.com'),
-('Diana', 'Evans', 30, 'F', 'diana.evans@@@XYZ.com'),
-('Ethan', 'Garcia', 24, 'M', 'ethan.garcia@@@XYZ.com');
+ALTER TABLE HELLO_WORLD ADD COLUMN AGE_OLD_NEW NUMBER;
